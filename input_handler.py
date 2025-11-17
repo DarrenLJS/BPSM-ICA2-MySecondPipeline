e93ade9d5d20ce76cbc42c3c9a55538dd7d0b429
DarrenLJS <darrenlim4567@gmail.com>
2025-11-17 20:27:24 +0000

    17 Nov 2025: Done input_handler. Updated main. Added .gitignore and requirements (.venv).

--- input_handler.py
+++ input_handler.py
@@ -1,8 +1,60 @@
-#!/usr/bin/python3
+#!.venv/bin/python3
 
-def get_input():
-    return protein_family, taxon_group
+import subprocess
+import requests
 
-def validate_input(protein_family, taxon_group):
-    pass
+def validate_protein(protein_family):
+    base_url = "https://www.ebi.ac.uk/ebisearch/ws/rest/pfam"
+    params = {
+        "query" : protein_family
+    }
+    headers = {"Accept" : "application/json"}
+    response = requests.get(base_url, params = params, headers = headers)
+    if not response.ok:
+        raise ValueError("Error querying EBI Search!")
+
+    data = response.json()
+    results = data.get("entries", [])
+    if not results:
+        raise ValueError(f"No result for {protein_family}!")
+
+    pfam_id = results[0].get("acc")
+    pfam_name = results[0].get("id")
+    return pfam_id, pfam_name
+
+def validate_taxon(taxon_group, email):
+    base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils"
+    params_esearch = {
+        "db" : "taxonomy", 
+        "term" : taxon_group, 
+        "retmax" : 20, 
+        "retmode" : "json", 
+        "email" : email
+    }
+    response_esearch = requests.get(f"{base_url}/esearch.fcgi", params = params_esearch)
+    if not response_esearch.ok:
+        raise ValueError("Error querying NCBI Taxonomy through esearch!")
+
+    search_result = response_esearch.json()
+    id_list = search_result.get("esearchresult", {}).get("idlist", [])
+    if not id_list:
+        raise ValueError(f"No result for {taxon_group}!")
+    
+    taxon_id = id_list[0]
+    params_esummary = {
+        "db" : "taxonomy", 
+        "id" : taxon_id, 
+        "retmode" : "json", 
+        "email" : email
+    }
+    response_esummary = requests.get(f"{base_url}/esummary.fcgi", params = params_esummary)
+    if not response_esummary.ok:
+        raise ValueError("Error querying NCBI Taxonomy through efetch!")
+
+    summary = response_esummary.json()
+    taxon_name = summary.get("result", {}).get(taxon_id, {}).get("scientificname")
+    if not taxon_name:
+        raise ValueError(f"No result for {taxon_id}!")
+
+    return taxon_id, taxon_name
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python3
+#!.venv/bin/python3
 
 import subprocess
-from input_handler import get_input, validate_input
+import re
+from input_handler import validate_protein, validate_taxon
 from fetch_sequence import run_esearch, run_efetch
 from parse_fasta import parse_fasta
 from conservation_analysis import temp_fasta, run_clustalo, run_plotcon
@@ -12,33 +13,54 @@ def main():
 
     while True:
         try:
-            protein_family, taxon_group = get_input()
-            validate_input(protein_family, taxon_group)
+            pfam_input = input("Protein family: ").strip()
+            pfam_id, pfam_name = validate_protein(pfam_input)
             break
-        except ValueError:
-            pass
-    
-    out_dir = f"{protein_family}_{taxon_group}"
-    subprocess.call(f"rm -rf {out_dir}")
-    subprocess.call(f"mkdir -p {out_dir}")
-    
-    ids_list = run_esearch(protein_family, taxon_group)
-    out_fetch = run_efetch(ids_list, out_dir)
-    
-    records = parse_fasta(out_dir, out_fetch)
-    
-    out_temp_fasta = temp_fasta(records, out_dir)
-    run_clustalo(out_dir, out_temp_fasta)
-    run_plotcon(out_dir)
+        except Exception as e:
+            print(f"Please input a valid protein family. {e}")
 
-    out_motifs = run_prosite_scan(out_dir, out_fetch)
-    parse_prosite_output(out_dir, out_motifs)
+    while True:
+        email = input("Your email (needed for NCBI URL request): ").strip()
+        if re.search(
+            r"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", 
+            email, re.IGNORECASE
+        ):
+            break
+        else:
+            print("Please input a valid email.")
+
+    while True:
+        try:
+            taxon_input = input("Taxon group: ").strip()
+            taxon_id, taxon_name = validate_taxon(taxon_input, email)
+            break
+        except Exception as e:
+            print(f"Please input a valid taxon group. {e}")
     
-    blastdb_name = f"{out_dir}_blastdb"
-    make_blast_db(out_dir, out_fetch, blastdb_name)
-    out_reference = select_blast_ref(records, out_dir)
-    out_blast = run_blast_search(out_dir, out_reference, blastdb_name)
-    parse_blast_output(out_dir, out_blast)
+    print(f"Your protein family: {pfam_name}\tPfam ID: {pfam_id}")
+    print(f"Your taxon group: {taxon_name}\tTaxon ID: {taxon_id}")
+
+    #out_dir = f"{protein_family}_{taxon_group}"
+    #subprocess.call(f"rm -rf {out_dir}")
+    #subprocess.call(f"mkdir -p {out_dir}")
+    
+    #ids_list = run_esearch(protein_family, taxon_group)
+    #out_fetch = run_efetch(ids_list, out_dir)
+    
+    #records = parse_fasta(out_dir, out_fetch)
+    
+    #out_temp_fasta = temp_fasta(records, out_dir)
+    #run_clustalo(out_dir, out_temp_fasta)
+    #run_plotcon(out_dir)
+
+    #out_motifs = run_prosite_scan(out_dir, out_fetch)
+    #parse_prosite_output(out_dir, out_motifs)
+    
+    #blastdb_name = f"{out_dir}_blastdb"
+    #make_blast_db(out_dir, out_fetch, blastdb_name)
+    #out_reference = select_blast_ref(records, out_dir)
+    #out_blast = run_blast_search(out_dir, out_reference, blastdb_name)
+    #parse_blast_output(out_dir, out_blast)
 
 
 if __name__ == "__main__":
